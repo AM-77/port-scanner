ba8482ceae0fc245f91b82a9cf1ed254e7500463
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-11-21 20:23:20 +0100

    Added inputs().

--- pScan.py
+++ pScan.py
@@ -7,4 +7,23 @@ getLogger("scapy.runtime").setLevel(ERROR)
 from scapy.all import *
 import sys
 
+#Getting input from the user
+def inputs():
+	
+	min_port = raw_input("[*] Min port number : ")
+	#add is_valid_port
+	
+	max_port = raw_input("[*] Max port number : ")
+	#add is_valid_port
+	
+	ip = raw_input("[*] Target ip address : ")
+	#add is_valid_ip
+	
+	#add target is_up 
+	
+	
+	ports_range = range(int(min_port), int(max_port)+1)
+	
+	return {"ip":ip, "ports":ports}
+	
 
