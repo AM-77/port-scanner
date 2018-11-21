afe97ae917098c24be4368c9167c03268c3a4c2e
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-11-21 21:15:16 +0100

    Added is_valid_port()

--- pScan.py
+++ pScan.py
@@ -10,11 +10,19 @@ import sys
 #Getting input from the user
 def inputs():
 	
-	min_port = raw_input("[*] Min port number : ")
-	#add is_valid_port
+	min_port = int(raw_input("[*] Min port number : "))
+	#Testin if is_valid_port
+	if is_valid_port(min_port) is not True:
+		print "[!] Unvalid port number."
+		#Exit the program with exit code = 1 / there was an error
+		sys.exit(1)
 	
-	max_port = raw_input("[*] Max port number : ")
-	#add is_valid_port
+	max_port = int(raw_input("[*] Max port number : "))
+	#Testing is_valid_port
+	if is_valid_port(min_port) is not True or min_port > max_port:
+		print "[!] Unvalid port number."
+		#Exit the program with exit code = 1 / there was an error
+		sys.exit(1)
 	
 	ip = raw_input("[*] Target ip address : ")
 	#add is_valid_ip
@@ -32,9 +40,20 @@ def is_up(ip):
 	
 	try:
 		#the target is up if it sends back a response
-		ping = sr1((IP((dst=ip)/ICMP))
+		ping = sr1(IP(dst=ip)/ICMP)
 		return True
 	
 	except Exception:
 		#An Exception will raise if the target was down
 		return False
+
+#Testing if the port is valid
+def is_valid_port(port):
+	
+	if isinstance(port,int) and port >= 0 and port <= 255 :
+		return True
+	else:
+		return False
+		
+if __name__ == "__main__":
+	print inputs()
